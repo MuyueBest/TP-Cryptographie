305c07db90daeac9f84ff9b7eecec8b5cd44e88f
Muyue 02 <102559201+MuyueBest@users.noreply.github.com>
2022-05-23 19:06:25 +0000

    Modification de la présentation générale et ajout de assert pour la vérification de fonction

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Montrer sur un exemple (message et clef de chiffrement de votre choix), que si on chiffre le message avec la clef et qu’on chiffre le message chiffré avec la même clef, on retrouve le message de départ en clair
+##Focntion non demandé
 
 def inverser_chaine_carcatere(chaine_caractere:str)->str:
     """inverse une chaine de carcatère"""
@@ -9,8 +9,10 @@ def inverser_chaine_carcatere(chaine_caractere:str)->str:
         i = i - 1
     return texte_inverse
 
+##Fonction demandé
+
 def convertit_texte_en_binaire(texte:str)->str:
-    """Convertit une chaine de caractère texte en binaire"""
+    """Convertit une chaine de caractère texte en base 2"""
     liste_texte_binaire = []
     texte_binaire = ""
     for caractere in texte:
@@ -22,7 +24,7 @@ def convertit_texte_en_binaire(texte:str)->str:
     return texte_binaire
 
 def convertit_binaire_vers_un_entier_en_base_10(chaine_binaire:str)->int:
-    """Convertit un binaire en entier"""
+    """Convertit base 2 en entier base 10"""
     entier = 0
     chaine_binaire = inverser_chaine_carcatere(chaine_binaire)
     for i in range(len(chaine_binaire)):
@@ -31,7 +33,7 @@ def convertit_binaire_vers_un_entier_en_base_10(chaine_binaire:str)->int:
     return entier
 
 def convertit_binaire_en_texte(binaire:str)->str:
-    """Convertit un binaire en texte"""
+    """Convertit base 2 en texte"""
     dico_lettre_texte = {}
     convertion_binaire = str()
     for i in range(0, len(binaire), 8):
@@ -40,8 +42,8 @@ def convertit_binaire_en_texte(binaire:str)->str:
         convertion_binaire += lettre
     return convertion_binaire
 
-def chiffre_xor(chaine_binaire:str,cle_binaire:str):
-    """ chiffre la chaine_binaire par un xor grace à la cle_binaire"""
+def chiffre_xor(chaine_binaire:str, cle_binaire:str)->str:
+    """Chiffre chaine_binaire avec un xor grâce à cle_binaire"""
     message_chiffre = ""
     for i in range(len(chaine_binaire)):
         a = int(chaine_binaire[i])
@@ -73,19 +75,26 @@ def modinv(e,n):
     pass
 
 
-## Test
+##Vérifications des fopnctions (ne suffit pas pour vérifier entierement une fonction)
 
+##Fonction non demandé
+    
 assert inverser_chaine_carcatere("NSI") == "ISN"
 
+##Fonction demandé
+
+assert convertit_texte_en_binaire("SPECIALITE NSI") =="0101001101010000010001010100001101001001010000010100110001001001010101000100010100100000010011100101001101001001"
+assert convertit_texte_en_binaire("TERM") ==  "01010100010001010101001001001101"
+
 assert convertit_binaire_vers_un_entier_en_base_10("01001110") == 78
+assert convertit_binaire_vers_un_entier_en_base_10('0001001011111111') == 4863
 
 assert convertit_binaire_en_texte("010011100101001101001001") == 'NSI'
+assert convertit_binaire_en_texte('00110010001000000100010001101001011001010111010101111000') == '2 Dieux'
 
-assert chiffre_xor("0101001101010000010001010100001101001001010000010100110001001001010101000100010100100000010011100101001101001001","01010100010001010101001001001101") == "0000011100010101000101110000111000011101000001000001111000000100000000000000000001110010000000110000011100001100"
+assert chiffre_xor("0101001101010000010001010100001101001001010000010100110001001001010101000100010100100000010011100101001101001001","01010100010001010101001001001101") == "0000011100010101000101110000111000011101000001000001111000000100000000000000000001110010000000110000011100001100" #Avec valeur = "SPECIALITE NSI", et clé = "TERM"
+assert chiffre_xor('00110010001000000100010001101001011001010111010101111000', '0101101001100101011100100110111100100000010101000111011101101111') == '01101000010001010011011000000110010001010010000100001111' #Avec valeur = "2 Dieux", et clé = "Zero Two"
 
-assert convertit_texte_en_binaire( "SPECIALITE NSI" ) =="0101001101010000010001010100001101001001010000010100110001001001010101000100010100100000010011100101001101001001"
-
-assert convertit_texte_en_binaire( "TERM" ) ==  "01010100010001010101001001001101"
 
 
 ##Exemple
